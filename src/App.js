07f58bea075c5cb6746aa35ea6493461d3aecc0a
thiagosaidm <thiag.said@gmail.com>
2022-12-18 13:20:33 -0300

    "Mudanças no Layout web"

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ function App() {
 
   const [info, setInfo] = useState({})
   const [text, setText] = useState('');
-  
-    
-    useEffect(() => {
-      
-      if (text) {
-        fetch(`${api}anime?filter[text]=${text}&page[limit]=1`)
-        .then((response) => response.json())
-          .then((response) => {
-            setInfo(response);
 
-           });
+
+  useEffect(() => {
+
+    if (text) {
+      fetch(`${api}anime?filter[text]=${text}&page[limit]=1`)
+        .then((response) => response.json())
+        .then((response) => {
+          setInfo(response);
+
+        });
     }
   }, [text]);
 
@@ -32,20 +32,23 @@ function App() {
           onChange={(search) => setText(search)}
           />
         {info.data && (
-          <ul className='animes-list'>
-            {info.data.map((anime) =>(
-              <li key={anime.id}>
-                <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle}/>
-                {anime.attributes.canonicalTitle}
-                <span>{anime.attributes.synopsis}</span>
-              </li>
-            ))}
-          </ul>
-          
+          <table className='animes-list'>
+            <tbody>
+              {info.data.map((anime) =>(
+                <tr key={anime.id}>  
+                  <td><span><img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle}/></span></td>
+                  <td className='title'>{anime.attributes.canonicalTitle}</td>
+                  <td className='sinop'>{anime.attributes.synopsis}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         )}
-
     </div>
   );
+  
+
+
 }
 
 export default App;
